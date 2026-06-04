c6c48f19e5beed45c24584b44880e33802f9c29c
hopzxje <hopnt1401@gmail.com>
2026-06-04 08:33:58 +0700

    Revert "Admin manager"

--- SWP.BLL/Interfaces/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using SWP.BLL.DTOs.Auth;
-
-namespace SWP.BLL.Interfaces;
-
-public interface IAuthService
-{
-    Task<AuthResponseDto> LoginAsync(LoginRequestDto request);
-    Task<UserInfoDto> GetProfileAsync(string id);
-}
